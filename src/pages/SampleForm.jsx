52a1b3ce44828431a4cc722050cba5b383fd9975
jackson-ruiz <jackson.ruiz@thoughtworks.com>
2022-10-25 15:39:27 -0500

    setup changes for module federation

---
+++ src/pages/SampleForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SampleForm = () => {
+  return <div>SampleForm</div>;
+};
+
+export default SampleForm;
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,16 @@
 const path = require("path");
+const { dependencies } = require("./package.json");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 module.exports = {
-  output: {
-    path: path.join(__dirname, "/dist"), // the bundle output path
-    filename: "bundle.js", // the name of the bundle
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html", // to import index.html file inside index.js
-    }),
-  ],
+  entry: "./src/index",
+  mode: "development",
   devServer: {
-    port: 3030, // you can change the port
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    port: 3001, // you can change the port
   },
   module: {
     rules: [
@@ -34,4 +32,31 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html", // to import index.html file inside index.js
+    }),
+    new ModuleFederationPlugin({
+      name: "mfChildren",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./SamplePage": "./src/pages/SamplePage.jsx",
+      },
+      shared: {
+        ...dependencies,
+        react: {
+          singleton: true,
+          requiredVersion: dependencies["react"],
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: dependencies["react-dom"],
+        },
+      },
+    }),
+  ],
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  target: "web",
 };
